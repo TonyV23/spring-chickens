7e88e2db8bf5959a27e73b0bdbced574a5ddd76e
viper23 <htonnycarlos@gmail.com>
2024-11-26 15:01:07 +0200

    added club service

--- src/main/java/com/rungroup/web/dto/ClubDto.java
+++ src/main/java/com/rungroup/web/dto/ClubDto.java
@@ -1,7 +1,12 @@
 package com.rungroup.web.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
+@Builder
 public class ClubDto {
     private int id;
     private String title;
---
+++ src/main/java/com/rungroup/web/service/impl/ClubServiceImpl.java
@@ -0,0 +1,36 @@
+package com.rungroup.web.service.impl;
+
+import com.rungroup.web.dto.ClubDto;
+import com.rungroup.web.models.Club;
+import com.rungroup.web.repository.ClubRepository;
+import com.rungroup.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClubServiceImpl implements ClubService {
+
+    private final ClubRepository clubRepository;
+
+    @Autowired
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map(this::mapToClubDto).collect(Collectors.toList());
+    }
+
+    private ClubDto  mapToClubDto(Club club) {
+        return ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+    }
+}
