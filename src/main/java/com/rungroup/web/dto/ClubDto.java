93a29fe5ad9047dc4d77d6e093aca9c5e0c79990
viper23 <htonnycarlos@gmail.com>
2024-11-26 13:02:33 +0200

    added repo and dto

---
+++ src/main/java/com/rungroup/web/dto/ClubDto.java
@@ -0,0 +1,11 @@
+package com.rungroup.web.dto;
+
+import java.time.LocalDateTime;
+
+public class ClubDto {
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
